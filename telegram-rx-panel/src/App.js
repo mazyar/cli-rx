820343231f817b4fea17d3f708a503b322d58bc4
mehrdad.mazyar <mehrdad.mazyar@gmail.com>
2020-02-16 10:28:27 +0330

    remove panel

--- telegram-rx-panel/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { FabricLayout } from "@bit/mazyar.m1-rx.core";
-import { Router } from "@reach/router";
-
-function App() {
-  return <FabricLayout>TEST</FabricLayout>;
-}
-
-export default App;
