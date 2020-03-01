a7800c9da2d61cba6a3b12e263325d5cf3302fb3
mehrdad.mazyar <mehrdad.mazyar@gmail.com>
2020-03-01 18:45:53 +0330

    zip file download and unzip

--- src/create.js
+++ src/create.js
@@ -1,19 +1,31 @@
 const fs = require("fs");
-const ncp = require("ncp").ncp;
+const https = require("https");
+// const ncp = require("ncp").ncp;
+const unzipper = require("unzipper");
 
-module.exports = ({ proj_name, log }) => {
-  const dir = `./${proj_name}`;
-  const source = "./rx-panel/";
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);   
-  }  
+const zip_source_file =
+  "https://raw.githubusercontent.com/mazyar/cli-rx/master/rx-panel.zip";
 
-
-  ncp(source, dir, err => {
-    if (err) {
-      return log(err);
-    }
-    log("create rx-panel done");
+module.exports = ({ proj_name }) => {
+  console.log(proj_name);
+  const file = fs.createWriteStream(`${proj_name}.zip`);
+  https.get(zip_source_file, response => {
+    response.pipe(file);
+    fs.createReadStream(`${proj_name}.zip`).pipe(
+      unzipper.Extract({ path:proj_name })
+    );
   });
 
+  // const dir = `./${proj_name}`;
+  // const source = "./rx-panel/";
+  // if (!fs.existsSync(dir)) {
+  //   fs.mkdirSync(dir);
+  // }
+
+  // ncp(source, dir, err => {
+  //   if (err) {
+  //     return log(err);
+  //   }
+  //   log("create rx-panel done");
+  // });
 };
